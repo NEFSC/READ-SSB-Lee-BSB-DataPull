ee54a492c8924e7a738d77794d4227e4c3540537
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2026-04-30 13:22:38 -0400

    wrapperize.

--- R_code/data_extraction_processing/extraction/commercial/01_extraction_wrapper.R
+++ R_code/data_extraction_processing/extraction/commercial/01_extraction_wrapper.R
@@ -13,10 +13,16 @@
 library("ROracle")
 library("glue")
 library("tidyverse")
+library("lubridate")
+
 library("here")
 library("conflicted")
 conflicts_prefer(dplyr::filter)
+conflicts_prefer(lubridate::year)
+conflicts_prefer(lubridate::month)
+conflicts_prefer(lubridate::week)
 conflicts_prefer(dplyr::summarise)
+conflicts_prefer(dplyr::arrange)
 
 here::i_am("R_code/data_extraction_processing/extraction/commercial/01_extraction_wrapper.R")
 
@@ -29,22 +35,20 @@ vintage_string <- format(Sys.Date())
 
 run_maryland_BSB      <- TRUE   # Module 1:   Maryland BSB by permit
 run_commercial_BSB <- TRUE   # Module 2 : Pull annual BSB commercial landings by permit, classifying
-run_cams_gears    <- TRUE   # Module 3 
-run_bsb_weekly        <- TRUE   # Module 3:   Geographic data processing
-run_dealers    <- TRUE   # Module 4:   Summary statistics/descriptive outputs
-run_sfsbsb_daily <- TRUE   # Module 5:   Hedonic price regression and prediction
-run_bsb_price_categories<- TRUE  # Module 6:   Discrete choice model estimation
-run_bsb_transactions      <- TRUE  # Unnamed:    QAQC of mata functions
-run_bsb_veslog      <- TRUE  # Unnamed:    QAQC of mata functions
-run_extractFRED      <- TRUE  # Unnamed:    QAQC of mata functions
-run_bsb_transactions      <- TRUE   # this is the most important data pull for the Prices in stock assessment Project
-run_bsb_veslog      <- TRUE  # Unnamed:    QAQC of mata functions
-run_permit_char      <- TRUE  # Unnamed:    QAQC of mata functions
-run_valid_fishery      <- TRUE  # Unnamed:    QAQC of mata functions
-run_dersource      <- TRUE  # Unnamed:    QAQC of mata functions
-run_fuel_prices      <- TRUE  # Unnamed:    QAQC of mata functions
-run_portlnd1      <- TRUE  # Unnamed:    QAQC of mata functions
-
+run_cams_gears    <- TRUE   # Module 3 : pulls cfg_gear
+run_bsb_weekly        <- TRUE   # Module 4:   Pull daily BSB commercial landings and aggregate to weekly 
+run_dealers    <- TRUE   # Module 5:   pull dealer attributes
+run_sfsbsb_daily <- TRUE   # Module 6:   Pull daily commercial sfsbsb at the trip-state-species level
+run_bsb_price_categories<- TRUE  # Module 7:   keyfile and daily landings
+run_bsb_veslog      <- TRUE  # Module 9:    Pull annual BSB commercial landings
+run_extractFRED      <- TRUE  # Module 10:    dfelators
+run_bsb_transactions      <- TRUE   # Module 11: main dataset. most important data pull for the Prices in stock assessment Project
+run_bsb_locations      <- TRUE  # Module 12:    compute landed weight average fishing lat and long for commercial
+run_permit_char      <- TRUE  # Module 13:    Build a permit-fishing_year panel of plan-cat, characteristics, and lobster traps
+run_valid_fishery      <- TRUE  # Module 14:    active plan/category combinations
+run_dersource      <- TRUE  # Module 15:    QA investigation of BSB commercial landings
+run_fuel_prices      <- TRUE  # Module 16:    Pull fuel prices for observer
+run_portlnd1      <- TRUE  # Module 17:    tack on landed ports
 
 
 
@@ -52,14 +56,28 @@ run_portlnd1      <- TRUE  # Unnamed:    QAQC of mata functions
 modules <- tibble::tribble(
   ~label,                              ~flag,
   "Module 1:   Maryland BSB by permit",  run_maryland_BSB,
-  "Module 1.5:     Commercial Permits",    run_commercial_BSB,
-  "Module 2:       Cleaning/Merging",  run_bsb_weekly,
-  "Module 3:       Mapping Prep",      run_dealers,
-  "Module 4:       Descriptive",       run_sfsbsb_daily,
-  "Module 5:       Price Modeling",    run_bsb_price_categories,
-  "Module 6:       Choice Modeling",   run_bsb_transactions,
-  "Module unnamed: QAQC mata",         run_bsb_veslog
-)
+  "Module 2:     Commercial Permits",    run_commercial_BSB,
+  "Module 3:       cfg_gear",      run_cams_gears,
+  "Module 4:       daily BSB commercial landings",       run_bsb_weekly,
+  "Module 5:   pull dealer attributes",run_dealers,
+  "Module 6:   Pull daily commercial sfsbsb at the trip-state-species level",run_sfsbsb_daily,
+  "Module 7:   keyfile and daily landings",run_bsb_price_categories,
+  "Module 9:    Pull annual BSB commercial landings",run_bsb_veslog,
+  "Module 10:    dfelators",run_extractFRED,
+  "Module 11: main dataset. most important data pull for the Prices in stock assessment Project",run_bsb_transactions,
+  "Module 12:    compute landed weight average fishing lat and long for commercial",run_bsb_locations,
+  "Module 13:    Build a permit-fishing_year panel of plan-cat, characteristics, and lobster traps",run_permit_char,
+  "Module 14:    active plan/category combinations",run_valid_fishery,
+  "Module 15:    QA investigation of BSB commercial landings",  run_dersource,
+  "Module 16:    Pull fuel prices for observer", run_fuel_prices,
+  "Module 17:    tack on landed ports", run_portlnd1
+  )
+
+      
+
+
+
+
 
 message("Execution Plan:")
 for (i in seq_len(nrow(modules))) {
@@ -83,6 +101,11 @@ if(run_cams_gears)   {
 if(run_bsb_weekly)   {    
   source(here("R_code", "data_extraction_processing","extraction","commercial", "bsb_weekly.R"))
 }
+
+# bsb weekly landings produces state instead of mys
+# The landings, value, and price are slightly different. my guess is because of the way the weekly aggregation is done 
+# produces year and week columns instead of weekly_date
+
 if(run_dealers)  {     
   source(here("R_code", "data_extraction_processing","extraction","commercial", "dealers.R"))
 }
@@ -92,26 +115,17 @@ if(run_sfsbsb_daily) {
 if(run_bsb_price_categories)  {
   source(here("R_code", "data_extraction_processing","extraction","commercial", "bsb_price_categories.R"))
 }
-if(run_bsb_locations) {       
-  source(here("R_code", "data_extraction_processing","extraction","commercial", "bsb_locations.R"))
-}
-if(run_bsb_transactions)   {    
-  source(here("R_code", "data_extraction_processing","extraction","commercial", "bsb_transactions.R"))
-}
 if(run_bsb_veslog)     { 
   source(here("R_code", "data_extraction_processing","extraction","commercial","bsb_veslog.R"))
 }
 if(run_extractFRED)      {
-  source(here("R_code", "data_extraction_processing","extraction","commercial", "extract_data_from_FRED.R"))
+  source(here("R_code", "data_extraction_processing","extraction", "extract_data_from_FRED.R"))
 }
 if(run_bsb_locations) {      
   source(here("R_code", "data_extraction_processing","extraction","commercial", "bsb_locations.R"))
 }
 if(run_bsb_transactions) {     
   source(here("R_code", "data_extraction_processing","extraction","commercial", "bsb_transactions.R")) # this is the most important data pull for the Prices in stock assessment Project
-}
-if(run_bsb_veslog)   { 
-  source(here("R_code", "data_extraction_processing","extraction","commercial", "bsb_veslog.R"))
 }
 if(run_permit_char) {      
   source(here("R_code", "data_extraction_processing","extraction","commercial", "permit_characteristics_extractions.R"))
